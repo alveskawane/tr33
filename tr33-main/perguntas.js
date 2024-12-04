6d79e8555817160c3cb67e37276005df8991098a
alveskawane <160686131+alveskawane@users.noreply.github.com>
2024-12-03 22:29:25 -0300

    Add files via upload

---
+++ tr33-main/perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+   'programação',
+    ' o que e python ',
+    'O python é uma linguagem de programação'
+)
+
+criaCartao(
+    'geografia',
+    'qual capital da frança ?',
+    'a capital da frança e paris'
+)
+
+criaCartao
+'programacão'
+' O que e uma função'
+'uma função e um bloco de codigos que executa alguma tarefa'
+
+
+    
